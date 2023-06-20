df6bd6f976fda2ca0b7fd160399197ab0bb53885
VoidAlpha <samrudhpk@gmail.com>
2023-06-20 13:32:19 +0530

    Second commit

---
+++ src/test/access_modifiers.java
@@ -0,0 +1,10 @@
+package test;
+
+public class access_modifiers {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/test/type_cast.java
@@ -0,0 +1,19 @@
+package test;
+
+public class type_cast {
+	public static void main(String[] args)
+	{
+		int a1=99;
+		double a2=99.32;
+		double a1_d= a1;
+		int a2_i = (int)a2;
+//		Here when we do Implicit type cast that is type casting from int to double  we will not be losing data 
+//		but in case of Explicit type cast that is type cast from double to int we will be losing the precision 
+//		so there maybe loss of data during Explicit type casting.
+		System.out.println("int value :"+ a1);
+		System.out.println("double value :"+ a1_d);
+		System.out.println("int value :"+ a2_i);
+		System.out.println("double value :"+ a2);
+	}
+
+}
